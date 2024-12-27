126d0e3b45f6d7bdc75857aad85bd9579d2e9b73
catgoldminer <marketing@catgoldminer.ai>
2024-12-27 12:06:59 +0700

    fix: handle bot err

--- src/bot.ts
+++ src/bot.ts
@@ -4,3 +4,18 @@ import * as process from 'process';
 const token = process.env.TELEGRAM_BOT_TOKEN!;
 
 export const bot = new TelegramBot(token, { polling: true });
+
+bot.on('polling_error', (error) => {
+    console.log('Polling error:', error.message);
+    
+    if (error.message.includes('terminated by other getUpdates request')) {
+        console.log('Trying to reconnect after 10 seconds...');
+        setTimeout(() => {
+            bot.stopPolling()
+                .then(() => {
+                    return bot.startPolling();
+                })
+                .catch(console.error);
+        }, 10000);
+    }
+});
